6c8b1922bdc48a3177b9025e44ee480ecb975065
Rajeshwari <rajeshwari91095@gmail.com>
2022-09-07 18:55:50 +0530

    staticfiles added

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = (BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
@@ -125,3 +125,4 @@ LOGIN_REDIRECT_URL = "todolist"
 LOGIN_URL = 'login'
 
 django_heroku.settings(locals())
+
